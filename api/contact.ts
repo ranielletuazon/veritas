1256c2dfc832af161dd5591063a1bd276d42f389
Dev - Ranielle Tuazon <143058053+ranielletuazon@users.noreply.github.com>
2026-07-16 21:51:35 +0800

    Update Resend API key initialization

--- api/contact.ts
+++ api/contact.ts
@@ -1,8 +1,8 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
-const resend = new Resend("re_SnnLB3NE_3ngvanaE6kS89Laj68qQpkTq");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (req.method !== "POST") {
